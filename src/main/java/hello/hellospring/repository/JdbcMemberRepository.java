f88c6c9a4e42eeded6447aae7306831284d14c2d
poohoh <kjh980328@gmail.com>
2022-07-10 19:39:24 +0900

    spring-integration-test

--- src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -1,40 +1,29 @@
 package hello.hellospring.repository;
-
 import hello.hellospring.domain.Member;
 import org.springframework.jdbc.datasource.DataSourceUtils;
-
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-
-public class JdbcMemberRepository implements MemberRepository{
-
+public class JdbcMemberRepository implements MemberRepository {
     private final DataSource dataSource;
-
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-
     @Override
     public Member save(Member member) {
-        String sql = "insert into member(name) values (?)";
-
+        String sql = "insert into member(name) values(?)";
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-
         try {
             conn = getConnection();
-            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
+            pstmt = conn.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);
             pstmt.setString(1, member.getName());
-
             pstmt.executeUpdate();
             rs = pstmt.getGeneratedKeys();
-
             if (rs.next()) {
                 member.setId(rs.getLong(1));
             } else {
@@ -47,23 +36,18 @@ public class JdbcMemberRepository implements MemberRepository{
             close(conn, pstmt, rs);
         }
     }
-
     @Override
     public Optional<Member> findById(Long id) {
-        String sql = "select * from member where id=?";
-
+        String sql = "select * from member where id = ?";
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-
         try {
             conn = getConnection();
             pstmt = conn.prepareStatement(sql);
             pstmt.setLong(1, id);
-
             rs = pstmt.executeQuery();
-
-            if (rs.next()) {
+            if(rs.next()) {
                 Member member = new Member();
                 member.setId(rs.getLong("id"));
                 member.setName(rs.getString("name"));
@@ -77,23 +61,42 @@ public class JdbcMemberRepository implements MemberRepository{
             close(conn, pstmt, rs);
         }
     }
-
     @Override
-    public Optional<Member> findByName(String name) {
-        String sql = "select * from member where name = ?";
-
+    public List<Member> findAll() {
+        String sql = "select * from member";
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            List<Member> members = new ArrayList<>();
+            while(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                members.add(member);
+            }
+            return members;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+    @Override
+    public Optional<Member> findByName(String name) {
+        String sql = "select * from member where name = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
         try {
             conn = getConnection();
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, name);
-
             rs = pstmt.executeQuery();
-
-            if (rs.next()) {
+            if(rs.next()) {
                 Member member = new Member();
                 member.setId(rs.getLong("id"));
                 member.setName(rs.getString("name"));
@@ -105,44 +108,12 @@ public class JdbcMemberRepository implements MemberRepository{
         } finally {
             close(conn, pstmt, rs);
         }
-
     }
-
-    @Override
-    public List<Member> findAll() {
-        String sql = "select * from member";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            conn = getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            rs = pstmt.executeQuery();
-
-            List<Member> members = new ArrayList<>();
-            while (rs.next()) {
-                Member member = new Member();
-                member.setId(rs.getLong("id"));
-                member.setName(rs.getString("name"));
-                members.add(member);
-            }
-
-            return members;
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        } finally {
-            close(conn, pstmt, rs);
-        }
-    }
-
     private Connection getConnection() {
         return DataSourceUtils.getConnection(dataSource);
     }
-
-    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
+    {
         try {
             if (rs != null) {
                 rs.close();
@@ -150,7 +121,6 @@ public class JdbcMemberRepository implements MemberRepository{
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         try {
             if (pstmt != null) {
                 pstmt.close();
@@ -158,16 +128,14 @@ public class JdbcMemberRepository implements MemberRepository{
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         try {
             if (conn != null) {
-                conn.close();
+                close(conn);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
     private void close(Connection conn) throws SQLException {
         DataSourceUtils.releaseConnection(conn, dataSource);
     }
